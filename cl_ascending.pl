b756ccc1b8239ffd73850e415d45a201bdd8f15e
Brian Rogers <brog45@gmail.com>
2019-12-15 23:32:54 -0600

    modify cl_descending to also require the the list be a list of numbers

--- cl_ascending.pl
+++ cl_ascending.pl
@@ -1,8 +1,11 @@
 :- use_module(library(chr)).
 
-:- chr_constraint cl_ascending/1.
+% :- chr_constraint cl_ascending/1.
+
+:- chr_type list(T) ---> [] ; [T | list(T)].
+:- chr_constraint cl_ascending(?list(number)).
 
 cl_ascending(L) <=> ground(L) 
     | L = []
     ; L = [_]
-    ; L = [A,B|Tail], B >= A, NewList = [B|Tail], cl_ascending(NewList).
+    ; L = [A,B|Tail], B >= A, cl_ascending([B|Tail]).
