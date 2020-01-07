7e9bf8445408003fa1965a4b1674a182e5ae5aa0
Brian Rogers <brog45@gmail.com>
2020-01-06 20:25:59 -0600

    add examples: must_be_int and short

---
+++ must_be_int.pl
@@ -0,0 +1,14 @@
+:- use_module(library(chr)).
+:- chr_constraint must_be_int/1, must_be_int/2.
+
+must_be_int(X) <=> ground(X) | integer(X).
+
+% must_be_int(X, Context) <=> 
+%     ground(X) | 
+%     (   integer(X) 
+%     ->  true 
+%     ;   throw(error(type_error(integer, X), Context))
+%     ).
+
+must_be_int(X, _) <=> ground(X), integer(X) | true.
+must_be_int(X, Context) <=> ground(X) | throw(error(type_error(integer, X), Context)).
