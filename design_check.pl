71fb65eb023918f7ada6e91201671a9b9057ee35
Brian Rogers <brog45@gmail.com>
2020-01-11 00:45:18 -0600

    design_check example

---
+++ design_check.pl
@@ -0,0 +1,12 @@
+:- use_module(library(chr)).
+:- set_prolog_flag(toplevel_mode, recursive).
+
+:- chr_constraint design_check/0, wire/2, pin_not_checked/1.
+
+%--------------
+
+design_check, wire(T, _) ==> pin_not_checked(T).
+design_check, pin_not_checked(T) \ pin_not_checked(T) <=> true. % get rid of duplicates
+design_check, wire(T, A), wire(T, B) \ pin_not_checked(T) <=> A \= B | true.
+design_check, pin_not_checked(T) <=> format('Pin ~q fails', [T]), fail.
+design_check <=> true.
