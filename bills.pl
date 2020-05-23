777e5abf6900c528aa37a243388ffc359cc552a1
Brian Rogers <brog45@gmail.com>
2020-05-22 23:18:06 -0500

    update bills.pl to print formatted output as it runs

--- bills.pl
+++ bills.pl
@@ -23,35 +23,41 @@ add_month(date(Y,M,D), NextDate) :-
         date_time_value(date, DT1, NextDate)
     ).
 
+print_ledger(date(Y,M,D), Description, Amount, Balance) :-
+    format('~|~`0t~d~2+/~|~`0t~d~2+/~d  ~|~w~32t~40+  ~|~32t~:d~6+  ~|~32t~:d~7+~n', [M, D, Y, Description, Amount, Balance]).
+
 :- chr_constraint balance(+), currdt(+), monthly(+,+,+), weekly(+,+,+), on(+,+,+), nextdt, untildt(+).
 
 advance(0) :- !.
 advance(Days) :- Days > 0, !, nextdt, Days1 is Days - 1, advance(Days1).
 
-% currdt(D) ==> format('Current date is now ~w~n', [D]).
 nextdt, currdt(Date) <=> 
     add_days(Date, 1, NewDate), 
     currdt(NewDate).
-currdt(Date) \ balance(Balance) <=> 
+currdt(date(Y,M,D)) \ balance(Balance) <=> 
     Balance =< 0 
-    | format('Date: ~w  Balance: ~w~n', [Date, Balance]), 
+    | format('Overdrawn on ~w/~w/~w with balance ~:d~n', [M, D, Y, Balance]), 
     fail.
 currdt(Date) \ balance(Balance), monthly(Date, Amount, Description) <=> 
     add_month(Date, NewDate), 
     Balance1 is Balance + Amount,
+    print_ledger(Date, Description, Amount, Balance1),
     balance(Balance1),
     monthly(NewDate, Amount, Description).
 currdt(Date) \ balance(Balance), weekly(Date, Amount, Description) <=> 
     add_days(Date, 7, NewDate), 
     Balance1 is Balance + Amount,
+    print_ledger(Date, Description, Amount, Balance1),
     balance(Balance1),
     weekly(NewDate, Amount, Description).
-currdt(Date) \ balance(Balance), on(Date, Amount, _Description) <=> 
+currdt(Date) \ balance(Balance), on(Date, Amount, Description) <=> 
     Balance1 is Balance + Amount,
+    print_ledger(Date, Description, Amount, Balance1),
     balance(Balance1).
 untildt(Date1), currdt(Date2) ==> Date1 \= Date2 | nextdt.
-untildt(Date), currdt(Date) <=> 
-    format('Finishing on ~w~n', [Date]),
+untildt(Date), currdt(Date), balance(Balance) <=> 
+    date(Y,M,D) = Date,
+    format('Finishing on ~w/~w/~w with balance ~:d~n', [M, D, Y, Balance]),
     true.
 
 go :-
